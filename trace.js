ad07543d607dd2c21b0c9a236aacc7795fd456f2
Raymond W. Ko <raymond.w.ko@gmail.com>
2021-01-18 21:23:25 -0500

    copy more fields from CallSite so source-map-support works correctly
    This unfortunately makes things slower, but I feel that correct stack
    traces are more important.

--- trace.js
+++ trace.js
@@ -58,26 +58,133 @@ function getCallSites(skip) {
 // so we'll leak memory.
 class CallSiteCopy {
   constructor(callSite) {
+    this._functionName = callSite.getFunctionName();
     this._fileName = callSite.getFileName();
     this._lineNumber = callSite.getLineNumber();
     this._columnNumber = callSite.getColumnNumber();
-    this._toString = callSite.toString(); // TODO this is slow
+    this._isConstructor = callSite.isConstructor();
+    this._isAsync = callSite.isAsync();
+    this._isNative = callSite.isNative();
+    this._isToplevel = callSite.isToplevel();
+    this._typeName = callSite.getTypeName();
+    this._methodName = callSite.getMethodName();
+    this._scriptNameOrSourceURL = callSite.getScriptNameOrSourceURL();
+    this._isEval = callSite.isEval();
+    this._evalOrigin = this.getEvalOrigin();
   }
 
+  getFunctionName() {
+    return this._functionName;
+  }
   getFileName() {
     return this._fileName;
   }
-
   getLineNumber() {
     return this._lineNumber;
   }
-
   getColumnNumber() {
     return this._columnNumber;
   }
+  isNative() {
+    return this._isNative;
+  }
+  isConstructor() {
+    return this._isConstructor;
+  }
+  isToplevel() {
+    return this._isToplevel;
+  }
+  getTypeName() {
+    return this._typeName;
+  }
+  getMethodName() {
+    return this._methodName;
+  }
+  getScriptNameOrSourceURL() {
+    return this._scriptNameOrSourceURL;
+  }
+  isEval() {
+    return this._isEval;
+  }
+  getEvalOrigin() {
+    return this._evalOrigin;
+  }
 
   toString() {
-    return this._toString;
+    let prefix = "";
+    if (this._isConstructor) {
+      prefix = "new ";
+    } else if (this._isAsync) {
+      prefix = "async ";
+    }
+    return prefix + this._functionName + " (" + this._fileName + ":" + this._lineNumber + ":" + this._columnNumber + ")";
+  }
+  // copied from https://github.com/evanw/node-source-map-support/blob/d0587453869fa19f35671f32f4e27514123877a7/source-map-support.js#L272
+  toString() {
+    var fileName;
+    var fileLocation = "";
+    if (this.isNative()) {
+      fileLocation = "native";
+    } else {
+      fileName = this.getScriptNameOrSourceURL();
+      if (!fileName && this.isEval()) {
+        fileLocation = this.getEvalOrigin();
+        fileLocation += ", ";  // Expecting source position to follow.
+      }
+
+      if (fileName) {
+        fileLocation += fileName;
+      } else {
+        // Source code does not originate from a file and is not native, but we
+        // can still get the source position inside the source string, e.g. in
+        // an eval string.
+        fileLocation += "<anonymous>";
+      }
+      var lineNumber = this.getLineNumber();
+      if (lineNumber != null) {
+        fileLocation += ":" + lineNumber;
+        var columnNumber = this.getColumnNumber();
+        if (columnNumber) {
+          fileLocation += ":" + columnNumber;
+        }
+      }
+    }
+
+    var line = "";
+    var functionName = this.getFunctionName();
+    var addSuffix = true;
+    var isConstructor = this.isConstructor();
+    var isMethodCall = !(this.isToplevel() || isConstructor);
+    if (isMethodCall) {
+      var typeName = this.getTypeName();
+      // Fixes shim to be backward compatable with Node v0 to v4
+      if (typeName === "[object Object]") {
+        typeName = "null";
+      }
+      var methodName = this.getMethodName();
+      if (functionName) {
+        if (typeName && functionName.indexOf(typeName) != 0) {
+          line += typeName + ".";
+        }
+        line += functionName;
+        if (methodName && functionName.indexOf("." + methodName) != functionName.length - methodName.length - 1) {
+          line += " [as " + methodName + "]";
+        }
+      } else {
+        line += typeName + "." + (methodName || "<anonymous>");
+      }
+    } else if (isConstructor) {
+      line += "new " + (functionName || "<anonymous>");
+    } else if (functionName) {
+      line += functionName;
+    } else {
+      line += fileLocation;
+      addSuffix = false;
+    }
+    if (addSuffix) {
+      line += " (" + fileLocation + ")";
+    }
+    return line;
   }
 }
 
